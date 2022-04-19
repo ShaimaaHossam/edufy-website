eb22ccbf7b993f0a3628c7ddc0f92001b0142f39
Hossam Dahshan <hossam.essam@munjz.com>
2022-04-19 20:54:44 +0200

    feat: added NotFound shared view

--- src/i18n/fallback/ar.js
+++ src/i18n/fallback/ar.js
@@ -4,6 +4,8 @@ const ar = {
 
   access_denied: "غير مسموح",
   access_denied_message: "لا تمتلك الصلاحيات لرؤية هذه الصفحة.",
+  not_found: "غير موجود",
+  not_found_message: "الصفحة المطلوبة غير موجودة الآن.",
 
   confirm: "تأكيد",
   go_back: "رجوع",
--- src/i18n/fallback/en.js
+++ src/i18n/fallback/en.js
@@ -4,6 +4,8 @@ const en = {
 
   access_denied: "Access Denied",
   access_denied_message: "You don't have permissions to view this page.",
+  not_found: "Not Found",
+  not_found_message: "The page you requested is not found now.",
 
   confirm: "Confirm",
   go_back: "Go Back",
