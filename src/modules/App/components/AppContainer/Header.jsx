f59e85d598e5e42462496b90a1649f685800878a
Hossam Dahshan <hossam.essam@munjz.com>
2022-04-20 22:10:05 +0200

    feat: added brand icon to Header

--- src/modules/App/components/AppContainer/Header.jsx
+++ src/modules/App/components/AppContainer/Header.jsx
@@ -1,5 +1,25 @@
+import { Box, Grid } from "@mui/material";
+
+import Link from "../../../../shared/components/Link";
+
+import brandLogoEn from "../../../../assets/logos/brand_logo_en.svg";
+
 function Header() {
-  return "HELLO MARAFIQ";
+  return (
+    <Grid container spacing={2} alignItems="center">
+      <Grid item sx={{ mr: "auto" }}>
+        <Link to="/">
+          <Box
+            component="img"
+            alt="munjz"
+            src={brandLogoEn}
+            display="block"
+            height={34}
+          />
+        </Link>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Header;
