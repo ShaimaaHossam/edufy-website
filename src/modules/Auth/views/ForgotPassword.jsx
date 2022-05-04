6d979a784423a0b84ea47a34ac0a5503a36d0b1c
MohamedEssam1695 <toma126542@gmail.com>
2022-05-04 17:10:33 +0200

    feat: added login reset & forgot password views to auth module

---
+++ src/modules/Auth/views/ForgotPassword.jsx
@@ -0,0 +1,103 @@
+import { useEffect } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import { useSelector, useDispatch } from "react-redux";
+import { forgetPassword,userSelector, clearState } from "../../../redux/userSlice";
+
+
+import { useTranslation } from "react-i18next";
+
+import * as Yup from "yup";
+import { useFormik } from "formik";
+
+import { Grid, Button, Typography, Paper } from "@mui/material";
+
+import TextInput from "../../../shared/components/inputs/TextInput";
+
+function ForgotPassword() {
+  const dispatch = useDispatch();
+  const { isFetching, isSuccess, isError, errorMessage } =
+    useSelector(userSelector);
+  const navigate = useNavigate();
+  
+
+  const { t } = useTranslation("Auth");
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    onSubmit: (values) => {
+      console.log("Form Data", values);
+      dispatch(forgetPassword(values));
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email formait").required("Required"),
+    }),
+  });
+  console.log("isSuccess", isSuccess)
+  useEffect(() => {
+    if (isError) {
+      dispatch(clearState());
+    }
+
+  }, [isError, isSuccess]);
+  return (
+    <Grid container spacing={3} padding={2} margin="auto">
+      <Grid
+        component="form"
+        onSubmit={formik.handleSubmit}
+        container
+        spacing={5}
+        margin="auto"
+      >
+        <Grid item container spacing={3}>
+          <Grid item xs={11}>
+            <TextInput
+              type="text"
+              name="email"
+              label="Email"
+              {...formik.getFieldProps("email")}
+              placeholder={t("loginEmail")}
+              error={formik.touched.email && !!formik.errors.email}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+            {isSuccess ? (
+              <Typography mt={2} color="success.main">
+                An Email was sent to your email address please check the email
+                to reset your password.
+              </Typography>
+            ) : null}
+          </Grid>
+        </Grid>
+
+        <Grid item xs={11} paddingBottom={3}>
+          <Button fullWidth type="submit">
+            <Typography>Reset password</Typography>
+          </Button>
+        </Grid>
+
+        <Grid item xs={11} paddingBottom={3}>
+          <Paper>
+            <Button
+              fullWidth
+              sx={{
+                backgroundColor: "#FFF",
+                "&:hover": { backgroundColor: "#FFF" },
+                color: "#242E42",
+              }}
+              onClick={() => {
+                navigate("/auth/login");
+              }}
+            >
+              <Typography color="primary.main">Back to login</Typography>
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default ForgotPassword;
---
+++ src/modules/Auth/views/ResetPassword.jsx
@@ -0,0 +1,151 @@
+import { useEffect } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+import { updatePassword,userSelector, clearState } from "../../../redux/userSlice";
+
+import { useNavigate } from "react-router-dom";
+
+import { useTranslation } from "react-i18next";
+
+import { mdiCheck } from "@mdi/js";
+
+import * as Yup from "yup";
+import { useFormik } from "formik";
+
+import {
+  SvgIcon,
+  Grid,
+  Button,
+  Typography,
+  Paper,
+  useTheme,
+} from "@mui/material";
+
+import TextInput from "../../../shared/components/inputs/TextInput";
+import PasswordInput from "../../../shared/components/inputs/PasswordInput";
+
+function ResetPasword() {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+  const { isFetching, isSuccess, isError, errorMessage } =
+    useSelector(userSelector);
+  let navigate = useNavigate();
+
+  const { t } = useTranslation("Auth");
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log("Form Data", values);
+      dispatch(updatePassword(values));
+
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email formait").required("Required"),
+      password: Yup.string().required("Required") 
+    }),
+  });
+
+
+  useEffect(() => {
+    if (isError) {
+      dispatch(clearState());
+    }
+  }, [isError, isSuccess]);
+
+  return (
+    <Grid container spacing={3} padding={2} margin="auto">
+      {isSuccess ? (
+        <Grid
+          item
+          container
+          spacing={3}
+          textAlign="center"
+          padding={4}
+          justifyContent="center"
+        >
+          <Grid item xs={11}>
+            <SvgIcon
+              sx={{
+                backgroundColor: theme.palette.success.main,
+                width: 129,
+                height: 129,
+                borderRadius: "100%",
+                color: "#FFF",
+                padding: 3,
+              }}
+              fontSize="small"
+            >
+              <path d={mdiCheck} />
+            </SvgIcon>
+          </Grid>
+
+          <Grid item xs={11}>
+            <Typography variant="h6" color="success.main">
+              Your password was reset successfully
+            </Typography>
+          </Grid>
+        </Grid>
+      ) : (
+        <Grid
+          component="form"
+          onSubmit={formik.handleSubmit}
+          container
+          spacing={5}
+          margin="auto"
+        >
+          <Grid item container spacing={3}>
+            <Grid item xs={11}>
+              <TextInput
+                type="text"
+                name="email"
+                label="Email"
+                {...formik.getFieldProps("email")}
+                placeholder={t("loginEmail")}
+                error={formik.touched.email && !!formik.errors.email}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+            </Grid>
+
+            <Grid item xs={11}>
+              <PasswordInput
+                name="password"
+                label="New Password"
+                placeholder="*******"
+                {...formik.getFieldProps("password")}
+                error={formik.touched.password && !!formik.errors.password}
+                helperText={formik.touched.password && formik.errors.password}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid item xs={11}>
+            <Button type="submit" fullWidth>
+              <Typography>Reset password</Typography>
+            </Button>
+          </Grid>
+        </Grid>
+      )}
+      <Grid item xs={10} margin="auto">
+        <Button
+          fullWidth
+          sx={{
+            backgroundColor: "#FFF",
+            "&:hover": { backgroundColor: "#FFF" },
+            color: "#242E42",
+          }}
+          onClick={() => {
+            navigate("/auth/login");
+          }}
+        >
+          <Typography color="primary.main">Back to login</Typography>
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default ResetPasword;
