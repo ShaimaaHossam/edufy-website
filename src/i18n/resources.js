c5e711fb8a4c1362c3a6412eb9955cc211cc7b59
Hossam Dahshan <hossam.essam@munjz.com>
2022-05-12 07:20:31 +0200

    refact: removed validations namespace

--- src/i18n/resources.js
+++ src/i18n/resources.js
@@ -1,15 +1,13 @@
 import fallback from "./fallback";
 import feedback from "./feedback";
-import validations from "./validations";
-import auth from "../modules/Auth/locales";
 
 import app from "../modules/App/locales";
+import auth from "../modules/Auth/locales";
 
 const resources = {
   ar: {
     fallback: fallback.ar,
     feedback: feedback.ar,
-    validations: validations.ar,
 
     app: app.ar,
     auth: auth.ar,
@@ -17,7 +15,6 @@ const resources = {
   en: {
     fallback: fallback.en,
     feedback: feedback.en,
-    validations: validations.en,
 
     app: app.en,
     auth: auth.en,
--- src/i18n/validations/ar.js
+++
@@ -1,5 +0,0 @@
-const ar = {
-  long_keyword: "كلمة البحث طويلة جدًا، الحد الأقصى ١٠٠ حرف.",
-};
-
-export default ar;
