923c587294cea370da150eccf95c208beeb4a94b
MohamedEssam1695 <toma126542@gmail.com>
2022-05-12 12:23:20 +0200

    feat: added titile for notification comp

--- src/modules/Settings/index.jsx
+++ src/modules/Settings/index.jsx
@@ -1,9 +1,13 @@
-import { Typography, Box, Paper } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import BasicTabs from "./components/SettingsTabPanel";
 function Settings() {
   return (
     <Box>
-      <Typography color="text.primary" variant="h6" sx={{ fontWeight: "bold",paddingBottom:2 }}>
+      <Typography
+        color="text.primary"
+        variant="h6"
+        sx={{ fontWeight: "bold", paddingBottom: 2 }}
+      >
         Settings
       </Typography>
       <BasicTabs />
