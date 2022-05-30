ff96b19b5037b6b2f7e166398d1bc6274401f8ac
Hossam Dahshan <hossam.essam@munjz.com>
2022-05-30 11:03:37 +0200

    refact: the redux store config

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+import { propertiesAPI } from "./services/properties";
+import { peopleAPI } from "./services/people";
+import { generalAPI } from "./services/general";
 
 import { authSlice } from "./slices/auth";
 
 export const store = configureStore({
   reducer: {
+    [propertiesAPI.reducerPath]: propertiesAPI.reducer,
+    [peopleAPI.reducerPath]: peopleAPI.reducer,
+    [generalAPI.reducerPath]: generalAPI.reducer,
+
     auth: authSlice.reducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([
+      propertiesAPI.middleware,
+      peopleAPI.middleware,
+      generalAPI.middleware,
+    ]),
 });
+
+setupListeners(store.dispatch);
