c68dc8b3ba95187c5b38174adef9cca010b9b770
Hossam Dahshan <hossam.essam@munjz.com>
2022-05-31 12:55:51 +0200

    feat: added units integrations to properties services

--- src/redux/services/properties.js
+++ src/redux/services/properties.js
@@ -5,31 +5,29 @@ import { customBaseQuery } from "../helpers/baseQuery";
 export const propertiesAPI = createApi({
   reducerPath: "propertiesAPI",
   refetchOnReconnect: true,
-  tagTypes: ["Properties"],
+  tagTypes: ["PROPERTY", "UNIT"],
   baseQuery: customBaseQuery,
   endpoints: (build) => ({
+    /** PROPERTIES SECTION **/
     getProperties: build.query({
       query: (queryParams) => ({ url: "/properties", params: queryParams }),
       transformResponse: (res, meta, queryParams) => ({
         data: res.data,
         meta: res.meta,
-        currentFilters: queryParams,
       }),
       providesTags: (res, err, queryParams) =>
         res
           ? [
-              ...res.data.map(({ id }) => ({ type: "Properties", id })),
-              { type: "Properties", id: "PARTIAL-LIST" },
+              ...res.data.map(({ id }) => ({ type: "PROPERTY", id })),
+              { type: "PROPERTY", id: "PARTIAL-LIST" },
             ]
-          : [{ type: "Properties", id: "PARTIAL-LIST" }],
+          : [{ type: "PROPERTY", id: "PARTIAL-LIST" }],
     }),
-
     getProperty: build.query({
       query: (id) => ({ url: `/properties/${id}` }),
       transformResponse: (res) => res.data,
-      providesTags: (res, err, id) => [{ type: "Properties", id }],
+      providesTags: (res, err, id) => [{ type: "PROPERTY", id }],
     }),
-
     addProperty: build.mutation({
       query: (data) => ({
         url: "/properties/create",
@@ -37,9 +35,9 @@ export const propertiesAPI = createApi({
         body: data,
       }),
       transformResponse: (res) => res.data,
-      invalidatesTags: [{ type: "Properties", id: "PARTIAL-LIST" }],
+      invalidatesTags: (res, error) =>
+        res ? [{ type: "PROPERTY", id: "PARTIAL-LIST" }] : [],
     }),
-
     updateProperty: build.mutation({
       query: ({ id, ...data }) => ({
         url: `/properties/update/${id}`,
@@ -47,20 +45,82 @@ export const propertiesAPI = createApi({
         body: data,
       }),
       transformResponse: (res) => res.data,
-      invalidatesTags: (result, error, { id }) => [
-        { type: "Properties", id },
-        { type: "Properties", id: "PARTIAL-LIST" },
-      ],
+      invalidatesTags: (res, error, { id }) =>
+        res
+          ? [
+              { type: "PROPERTY", id },
+              { type: "PROPERTY", id: "PARTIAL-LIST" },
+            ]
+          : [],
     }),
 
+    /** UNITS SECTION **/
+    getUnits: build.query({
+      query: (queryParams) => ({ url: "/units", params: queryParams }),
+      transformResponse: (res, meta, queryParams) => ({
+        data: res.data,
+        meta: res.meta,
+      }),
+      providesTags: (res, err, queryParams) =>
+        res
+          ? [
+              ...res.data.map(({ id }) => ({ type: "UNIT", id })),
+              { type: "UNIT", id: "PARTIAL-LIST" },
+            ]
+          : [{ type: "UNIT", id: "PARTIAL-LIST" }],
+    }),
+    getUnit: build.query({
+      query: (id) => ({ url: `/units/${id}` }),
+      transformResponse: (res) => res.data,
+      providesTags: (res, err, id) => [{ type: "UNIT", id }],
+    }),
+    addUnit: build.mutation({
+      query: (data) => ({
+        url: "/units/create",
+        method: "POST",
+        body: data,
+      }),
+      transformResponse: (res) => res.data,
+      invalidatesTags: (res, error, { id }) =>
+        res ? [{ type: "UNIT", id: "PARTIAL-LIST" }] : [],
+    }),
+    updateUnit: build.mutation({
+      query: ({ id, ...data }) => ({
+        url: `/unit/update/${id}`,
+        method: "PATCH",
+        body: data,
+      }),
+      transformResponse: (res) => res.data,
+      invalidatesTags: (res, error, { id }) =>
+        res
+          ? [
+              { type: "PROPERTY", id },
+              { type: "PROPERTY", id: "PARTIAL-LIST" },
+            ]
+          : [],
+    }),
+
+    /** PROPERTY TYPES SECTION **/
     getAllPropertyTypes: build.query({
       query: () => ({ url: "/properties/types", params: { listing: "1" } }),
       transformResponse: (res) => res.data,
     }),
-
     getAllPropertySubtypes: build.query({
       query: (typeID) => ({
         url: `/properties/types/${typeID}`,
+        params: { listing: "1" },
+      }),
+      transformResponse: (res) => res.data,
+    }),
+
+    /** UNIT TYPES SECTION **/
+    getAllUnitTypes: build.query({
+      query: () => ({ url: "/units/types", params: { listing: "1" } }),
+      transformResponse: (res) => res.data,
+    }),
+    getAllUnitSubtypes: build.query({
+      query: (typeID) => ({
+        url: `/units/types/${typeID}`,
         params: { listing: "1" },
       }),
       transformResponse: (res) => res.data,
@@ -74,6 +134,14 @@ export const {
   useAddPropertyMutation,
   useUpdatePropertyMutation,
 
+  useGetUnitsQuery,
+  useGetUnitQuery,
+  useAddUnitMutation,
+  useUpdateUnitMutation,
+
   useGetAllPropertyTypesQuery,
   useGetAllPropertySubtypesQuery,
+
+  useGetAllUnitTypesQuery,
+  useGetAllUnitSubtypesQuery,
 } = propertiesAPI;
