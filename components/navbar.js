6cf324cd2cbb8a77bcc2ea9a5550cf53c7d3e9cb
hagarts-site <40139825+ShaimaaHossam@users.noreply.github.com>
2022-06-25 12:14:33 +0200

    Fixed build bugs

--- components/navbar.js
+++ components/navbar.js
@@ -21,57 +21,57 @@ export default function Navbar() {
   }
   return (
     <>
-      <nav class="  px-2 sm:px-4 py-2.5  bg-gray-900 relative">
-        <div class="container flex flex-wrap justify-between items-center mx-auto">
+      <nav className="  px-2 sm:px-4 py-2.5  bg-gray-900 relative">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
           <div className="ml-16">
             <Logo />
           </div>
           <button
             onClick={switchFlag}
             type="button"
-            class="inline-flex items-center p-2 mr-6 text-sm text-gray-500 rounded-lg lg:hidden focus:outline-none focus:ring-2  dark:text-gray-400 "
+            className="inline-flex items-center p-2 mr-6 text-sm text-gray-500 rounded-lg lg:hidden focus:outline-none focus:ring-2  dark:text-gray-400 "
             aria-controls="mobile-menu"
             aria-expanded="false"
           >
             <svg
-              class="w-6 h-6"
+              className="w-6 h-6"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
             <svg
-              class="hidden w-6 h-6"
+              className="hidden w-6 h-6"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </button>
           <div
-            class={
+            className={
               flag == 1
                 ? "w-full  absolute top-16 left-0 bg-gray-900 z-20 pb-6"
                 : "hidden lg:block"
             }
             id="mobile-menu"
           >
-            <ul class="flex flex-col lg:flex-row mt-4 md:mt-0 md:text-sm ">
+            <ul className="flex flex-col lg:flex-row mt-4 md:mt-0 md:text-sm ">
               <li>
                 <Link href="/" passHref>
                   <a
-                  onClick={()=>switchFlag()}
-                    class="block py-2 pr-4 pl-3 text-gray-200 border-b lg:border-0 border-gray-100  "
+                  
+                    className="block py-2 pr-4 pl-3 text-gray-200 border-b lg:border-0 border-gray-100  "
                     aria-current="page"
                   >
                     Home
@@ -81,9 +81,9 @@ export default function Navbar() {
               <li>
                 <Link href="#about" passHref>
                 <a
-                onClick={()=>switchFlag()}
+                
                   href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-100 lg:border-0  "
+                  className="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-100 lg:border-0  "
                 >
                   About
                 </a>
@@ -93,9 +93,9 @@ export default function Navbar() {
               <li>
                 <Link href="/pricing" passHref>
                 <a
-                onClick={()=>switchFlag()}
+                
                   href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-100 lg:border-0  "
+                  className="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-100 lg:border-0  "
                 >
                   Pricing
                 </a>
@@ -104,9 +104,9 @@ export default function Navbar() {
               <li>
                 <Link href="#contact" passHref>
                 <a
-                onClick={()=>switchFlag()}
+                
                   href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-100 lg:border-0  "
+                  className="block py-2 pr-4 pl-3 text-gray-200 border-b border-gray-100 lg:border-0  "
                 >
                   Contact
                 </a>
@@ -116,9 +116,9 @@ export default function Navbar() {
               <li>
                 <Link href="/sign-up" passHref>
                 <a
-                onClick={()=>switchFlag()}
+                
                   href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-200  lg:border-l-2 "
+                  className="block py-2 pr-4 pl-3 text-gray-200  lg:border-l-2 "
                 >
                   Sign Up
                 </a>
--- pages/pricing.js
+++ pages/pricing.js
@@ -10,172 +10,172 @@ export default function Pricing() {
         </div>
 
         <div className="md:flex justify-around flex-wrap">
-        <div class="rounded-lg shadow-lg overflow-hidden mb-4">
-    <div class="px-6 py-8 bg-white dark:bg-gray-600 sm:p-10 sm:pb-6">
-        <div class="flex justify-center">
-            <span class="inline-flex px-4 py-1 dark:text-white rounded-full text-sm leading-5 font-semibold tracking-wide uppercase">
+        <div className="rounded-lg shadow-lg overflow-hidden mb-4">
+    <div className="px-6 py-8 bg-white dark:bg-gray-600 sm:p-10 sm:pb-6">
+        <div className="flex justify-center">
+            <span className="inline-flex px-4 py-1 dark:text-white rounded-full text-sm leading-5 font-semibold tracking-wide uppercase">
                 Standard Plan
             </span>
         </div>
-        <div class="mt-4 flex justify-center text-6xl leading-none font-extrabold dark:text-white">
+        <div className="mt-4 flex justify-center text-6xl leading-none font-extrabold dark:text-white">
             $10
-            <span class="ml-1 pt-8 text-2xl leading-8 font-medium text-gray-500 dark:text-gray-400">
+            <span className="ml-1 pt-8 text-2xl leading-8 font-medium text-gray-500 dark:text-gray-400">
                 /month
             </span>
         </div>
     </div>
-    <div class="px-6 pt-6 pb-8 bg-white dark:bg-gray-700 sm:p-10 sm:pt-6">
+    <div className="px-6 pt-6 pb-8 bg-white dark:bg-gray-700 sm:p-10 sm:pt-6">
         <ul>
-            <li class="mt-4 flex items-start">
-                <div class="flex-shrink-0">
-                    <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+            <li className="mt-4 flex items-start">
+                <div className="flex-shrink-0">
+                    <svg className="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7">
                         </path>
                     </svg>
                 </div>
-                <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
+                <p className="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
                     $10/month 
                 </p>
             </li>
-            <li class="mt-4 flex items-start">
-                <div class="flex-shrink-0">
-                    <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+            <li className="mt-4 flex items-start">
+                <div className="flex-shrink-0">
+                    <svg className="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7">
                         </path>
                     </svg>
                 </div>
-                <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
+                <p className="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
                     Unlimited number of projects
                 </p>
             </li>
-            <li class="mt-4 flex items-start">
-                <div class="flex-shrink-0">
-                    <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+            <li className="mt-4 flex items-start">
+                <div className="flex-shrink-0">
+                    <svg className="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7">
                         </path>
                     </svg>
                 </div>
-                <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
+                <p className="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
                     Cancel anytime
                 </p>
             </li>
         </ul>
-        <div class="mt-6 rounded-md shadow">
-            <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+        <div className="mt-6 rounded-md shadow">
+            <a href="#" className="flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
                 Start plan
             </a>
         </div>
     </div>
 </div>
-<div class="rounded-lg shadow-lg overflow-hidden mb-4">
-    <div class="px-6 py-8 bg-white dark:bg-gray-600 sm:p-10 sm:pb-6">
-        <div class="flex justify-center">
-            <span class="inline-flex px-4 py-1 dark:text-white rounded-full text-sm leading-5 font-semibold tracking-wide uppercase">
+<div className="rounded-lg shadow-lg overflow-hidden mb-4">
+    <div className="px-6 py-8 bg-white dark:bg-gray-600 sm:p-10 sm:pb-6">
+        <div className="flex justify-center">
+            <span className="inline-flex px-4 py-1 dark:text-white rounded-full text-sm leading-5 font-semibold tracking-wide uppercase">
                 Standard Plan
             </span>
         </div>
-        <div class="mt-4 flex justify-center text-6xl leading-none font-extrabold dark:text-white">
+        <div className="mt-4 flex justify-center text-6xl leading-none font-extrabold dark:text-white">
             $10
-            <span class="ml-1 pt-8 text-2xl leading-8 font-medium text-gray-500 dark:text-gray-400">
+            <span className="ml-1 pt-8 text-2xl leading-8 font-medium text-gray-500 dark:text-gray-400">
                 /month
             </span>
         </div>
     </div>
-    <div class="px-6 pt-6 pb-8 bg-white dark:bg-gray-700 sm:p-10 sm:pt-6">
+    <div className="px-6 pt-6 pb-8 bg-white dark:bg-gray-700 sm:p-10 sm:pt-6">
         <ul>
-            <li class="mt-4 flex items-start">
-                <div class="flex-shrink-0">
-                    <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+            <li className="mt-4 flex items-start">
+                <div className="flex-shrink-0">
+                    <svg className="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7">
                         </path>
                     </svg>
                 </div>
-                <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
+                <p className="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
                     $10/month
                 </p>
             </li>
-            <li class="mt-4 flex items-start">
-                <div class="flex-shrink-0">
-                    <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+            <li className="mt-4 flex items-start">
+                <div className="flex-shrink-0">
+                    <svg className="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7">
                         </path>
                     </svg>
                 </div>
-                <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
+                <p className="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
                     Unlimited number of projects
                 </p>
             </li>
-            <li class="mt-4 flex items-start">
-                <div class="flex-shrink-0">
-                    <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+            <li className="mt-4 flex items-start">
+                <div className="flex-shrink-0">
+                    <svg className="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7">
                         </path>
                     </svg>
                 </div>
-                <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
+                <p className="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
                     Cancel anytime
                 </p>
             </li>
         </ul>
-        <div class="mt-6 rounded-md shadow">
-            <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+        <div className="mt-6 rounded-md shadow">
+            <a href="#" className="flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
                 Start plan
             </a>
         </div>
     </div>
 </div>
-<div class="rounded-lg shadow-lg overflow-hidden mb-4">
-    <div class="px-6 py-8 bg-white dark:bg-gray-600 sm:p-10 sm:pb-6">
-        <div class="flex justify-center">
-            <span class="inline-flex px-4 py-1 dark:text-white rounded-full text-sm leading-5 font-semibold tracking-wide uppercase">
+<div className="rounded-lg shadow-lg overflow-hidden mb-4">
+    <div className="px-6 py-8 bg-white dark:bg-gray-600 sm:p-10 sm:pb-6">
+        <div className="flex justify-center">
+            <span className="inline-flex px-4 py-1 dark:text-white rounded-full text-sm leading-5 font-semibold tracking-wide uppercase">
                 Standard Plan
             </span>
         </div>
-        <div class="mt-4 flex justify-center text-6xl leading-none font-extrabold dark:text-white">
+        <div className="mt-4 flex justify-center text-6xl leading-none font-extrabold dark:text-white">
             $10
-            <span class="ml-1 pt-8 text-2xl leading-8 font-medium text-gray-500 dark:text-gray-400">
+            <span className="ml-1 pt-8 text-2xl leading-8 font-medium text-gray-500 dark:text-gray-400">
                 /month
             </span>
         </div>
     </div>
-    <div class="px-6 pt-6 pb-8 bg-white dark:bg-gray-700 sm:p-10 sm:pt-6">
+    <div className="px-6 pt-6 pb-8 bg-white dark:bg-gray-700 sm:p-10 sm:pt-6">
         <ul>
-            <li class="mt-4 flex items-start">
-                <div class="flex-shrink-0">
-                    <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+            <li className="mt-4 flex items-start">
+                <div className="flex-shrink-0">
+                    <svg className="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7">
                         </path>
                     </svg>
                 </div>
-                <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
+                <p className="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
                     $10/month
                 </p>
             </li>
-            <li class="mt-4 flex items-start">
-                <div class="flex-shrink-0">
-                    <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+            <li className="mt-4 flex items-start">
+                <div className="flex-shrink-0">
+                    <svg className="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7">
                         </path>
                     </svg>
                 </div>
-                <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
+                <p className="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
                     Unlimited number of projects
                 </p>
             </li>
-            <li class="mt-4 flex items-start">
-                <div class="flex-shrink-0">
-                    <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+            <li className="mt-4 flex items-start">
+                <div className="flex-shrink-0">
+                    <svg className="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7">
                         </path>
                     </svg>
                 </div>
-                <p class="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
+                <p className="ml-3 text-base leading-6 text-gray-700 dark:text-gray-200">
                     Cancel anytime
                 </p>
             </li>
         </ul>
-        <div class="mt-6 rounded-md shadow">
-            <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+        <div className="mt-6 rounded-md shadow">
+            <a href="#" className="flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
                 Start plan
             </a>
         </div>
