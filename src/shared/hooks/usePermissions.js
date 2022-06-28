4a09be61e68f4b006cdb6519235d2b73b8153854
MohamedEssam1695 <toma126542@gmail.com>
2022-06-28 22:18:09 +0200

    feat: added usePermissions hook to the app

---
+++ src/shared/hooks/usePermissions.js
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux";
+import { authSelector } from "../../redux/slices/auth";
+
+/**
+ * return all permissions when there is no argumnts
+ *
+ * when pass array of strings or single string it will it will return that specific permissions
+ *
+ * @param  keys
+ */
+
+function usePermissions(keys) {
+  const {
+    permissions
+  } = useSelector(authSelector);
+
+  if (!keys) {
+    return permissions;
+  }
+
+  if (typeof keys === "string") {
+    return permissions[keys] || {};
+  }
+
+  if (keys instanceof Array) {
+    const selectedPermissions = keys.reduce(
+      (acc, key) => ({ ...acc, [key]: permissions[key] ?? {} }),
+      {}
+    );
+    return selectedPermissions;
+  }
+  return {};
+}
+
+export default usePermissions;
