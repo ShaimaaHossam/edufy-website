790187a0230d86e6c1e90952fc557b56daac0989
Hossam Dahshan <hossam.essam@munjz.com>
2022-07-01 14:05:36 +0200

    feat: added WalletTransTable component

--- src/helpers/datetime.js
+++ src/helpers/datetime.js
@@ -1,5 +1,27 @@
+import enLocale from "date-fns/locale/en-US";
+import arLocale from "date-fns/locale/ar-SA";
+
 import { format } from "date-fns";
 
-export const formatDate = (date, formatStr = "yyyy-MM-dd") => {
-  return format(date, formatStr);
+const datetimeLocalesMap = { en: enLocale, ar: arLocale };
+
+export const formats = {
+  be: "yyyy-MM-dd",
+  dateShort: "dd/MM/yyyy",
+
+  dateTimeShort: "dd/MM/yyyy hh:mmaaa",
+};
+
+export const formatDate = (date, formatStr = formats.be, locale) => {
+  if (!date) return null;
+
+  return locale
+    ? format(new Date(date), formatStr, { locale: datetimeLocalesMap[locale] })
+    : format(new Date(date), formatStr);
+};
+
+export const toTimeZone = (date, tz = "UTC") => {
+  if (!date) return null;
+
+  return `${date} ${tz}`;
 };
---
+++ src/modules/Accounting/components/WalletTransTable.jsx
@@ -0,0 +1,113 @@
+import { useSelector, useDispatch } from "react-redux";
+import { filtersSelector, setFilters } from "../state/walletSlice";
+
+import { useGetWalletTransactionsQuery } from "../../../redux/services/accounting";
+
+import { useTranslation } from "react-i18next";
+
+import { Typography, Link } from "@mui/material";
+import { mdiMinus } from "@mdi/js";
+
+import Table from "../../../shared/components/Table";
+import Icon from "../../../shared/components/Icon";
+
+import NoContent from "../../../shared/views/NoContent";
+
+import { toTimeZone, formatDate, formats } from "../../../helpers/datetime";
+
+function WalletTransTable() {
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation("accounting");
+
+  const dispatch = useDispatch();
+  const { filters } = useSelector(filtersSelector);
+
+  const { isLoading, data: transactions } =
+    useGetWalletTransactionsQuery(filters);
+
+  const tableLabels = [
+    t("type"),
+    t("date"),
+    t("balanceBefore"),
+    t("amount"),
+    t("balanceAfter"),
+    t("description"),
+    t("remarks"),
+  ];
+
+  const tableData = transactions?.data?.map((item) => ({
+    id: item.id,
+    active: true,
+    clickable: false,
+    rowCells: [
+      <Typography component="span" variant="body2" color="primary">
+        {t(item.type)}
+      </Typography>,
+
+      <Typography component="span" variant="body2">
+        {formatDate(toTimeZone(item.created_at), formats.dateShort, language)}
+      </Typography>,
+
+      <Typography component="span" variant="body2">
+        {item.balance_before.toFixed(2)} {t("sr")}
+      </Typography>,
+
+      <Typography component="span" variant="body2">
+        {item.amount.toFixed(2)} {t("sr")}
+      </Typography>,
+
+      <Typography component="span" variant="body2">
+        {item.balance_after.toFixed(2)} {t("sr")}
+      </Typography>,
+
+      <Typography component="span" variant="body2">
+        {t(item.description)}
+      </Typography>,
+
+      item.remarks ? (
+        item.remarks.url ? (
+          <Link
+            variant="body2"
+            underline="hover"
+            href={item.remarks.url}
+            target="_blank"
+            rel="noopener"
+          >
+            {item.remarks.title}
+          </Link>
+        ) : (
+          <Typography component="span" variant="body2">
+            {item.remarks.title}
+          </Typography>
+        )
+      ) : (
+        <Icon icon={mdiMinus} size="medium" color="action" />
+      ),
+    ],
+  }));
+
+  if (isLoading) return null;
+
+  return !!tableData?.length ? (
+    <Table
+      tableLabel="wallet transactions list"
+      headLabels={tableLabels}
+      rowsData={tableData}
+      metadata={{
+        total: transactions.meta.total,
+        pages: transactions.meta.lastPage,
+        perPage: transactions.meta.perPage,
+        currentPage: transactions.meta.currentPage,
+      }}
+      onPageChange={(page, perPage) =>
+        dispatch(setFilters({ ...filters, page, perPage }))
+      }
+    />
+  ) : (
+    <NoContent />
+  );
+}
+
+export default WalletTransTable;
