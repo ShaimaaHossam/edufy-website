828375cd9312a3a3456682816f9bfd48667f032b
Mhmd Essam <mohammed.essam@munjez.com>
2022-07-06 13:40:42 +0200

    feat: added ar locales

--- src/modules/Dashboard/locales/ar.js
+++ src/modules/Dashboard/locales/ar.js
@@ -1,3 +1,32 @@
-const ar = {};
+const ar = {
+    totalOrders:"الطلبات بالكامل",
+    openOrders:"الطلبات المفتوحه",
+    completedOrders:"الطلبات المكتمله",
+    cancelledOrders:"الطلبات الملغاه",
+    accountCapacity:"الرصيد الاساسي ",
+    deposit:"الايداع",
+    availableBlance:"الرصيد المتوفر",
+    totalSpending:"إجمالي الإنفاق",
+    serviceType:"الوظائف حسب نوع الخدمة",
+
+    propertyName:"اسم العقار ",
+    service:"الخدمة",
+    subService:"الخدمة الفرعية",
+    numberOfOrders:"عدد الطلبات ",
+    vistOrders:"طلبات الزيارة بالمواد",
+    cost:"التكلفة الإجمالية",
+    unit:"الوحده",
+    createdBy:"بواسطة",
+    materialSpending:"الإنفاق المادي",
+    propertyStats:"احصائيات العقار",
+
+    byProperty:"العقار",
+    byUserRole:"دور المستخدم",
+    noTypes:"لايوجد",
+    sr:"ريال"
+
+
+
+};
 
 export default ar;
--- src/modules/Dashboard/locales/en.js
+++ src/modules/Dashboard/locales/en.js
@@ -1,3 +1,31 @@
-const en = {};
+const en = {
+    totalOrders:"Total Orders",
+    openOrders:"Open Orders",
+    completedOrders:"Completed Orders",
+    cancelledOrders:"Cancelled Orders",
+    accountCapacity:"Account Capacity",
+    deposit:"Deposit",
+    availableBlance:"Available Blance",
+    totalSpending:"Total Spending",
+    serviceType:"Jobs By Service Type",
+
+    propertyName:"Property Name",
+    service:"Service",
+    subService:"Sub Service",
+    numberOfOrders:"Number of orders ",
+    vistOrders:"Visit orders with material",
+    cost:"Total Cost",
+    unit:"Unit",
+    createdBy:"By",
+    materialSpending:"Material Spending",
+    propertyStats:"Property Stats",
+
+    byProperty:"Property",
+    byUserRole:"User Role",
+    noTypes:"No Types",
+    sr:"SR"
+
+    
+};
 
 export default en;
