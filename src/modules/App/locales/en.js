59f0fab66292060b63887208a83bac2b3c386fb5
Hossam Dahshan <hossam.essam@munjz.com>
2022-07-07 16:12:54 +0200

    feat: added more translations to App module

--- src/modules/App/locales/en.js
+++ src/modules/App/locales/en.js
@@ -21,13 +21,35 @@ const en = {
   rolesAndPermissions: "Roles And Permissions",
   servicesSettings: "Services Settings",
 
-  my_profile: "My Profile",
+  myProfile: "My Profile",
+
   logout: "Logout",
 
   add: "Add",
-  add_user: "Add User",
-  add_order: "Add Order",
-  add_property: "Add Property",
+  addProperty: "Add Property",
+  addCustomer: "Add Customer",
+  addTeamMember: "Add Team Member",
+  addMaintenanceOrder: "Add Maintenance Order",
+  addCleaningOrder: "Add Cleaning Order",
+
+  order_submitted: "Order #{{item.item_name}} is confirmed.",
+  visit_submitted: "Visit #{{item.item_name}} is confirmed.",
+  visit_reminder: "You have a visit #{{item.item_name}} scheduled for today.",
+  order_assigned: "SP is assigned for order #{{item.item_name}}.",
+  order_started: "Order #{{item.item_name}} has started and in progress.",
+  material_quote_added:
+    "Material quotation added for order #{{item.item_name}}, an action is required within 4 hours.",
+  sp_finished:
+    "SP job is done on order #{{item.item_name}}, please confirm the job is complete.",
+  order_finished: "Your order #{{item.item_name}} is completed.",
+  order_canceled: "Your order #{{item.item_name}} is canceled.",
+
+  invoice_added: "New invoice #{{item.item_name}} has been added.",
+  invoice_paid: "Invoice #{{item.item_name}} is paid successfully.",
+  credit_note_added: "Credit note #{{item.item_name}} has been added.",
+  wallet_recharge_faild: "Wallet charge #{{item.item_name}} faild.",
+
+  freshdesk_update: "{{item.extra_info}}",
 };
 
 export default en;
