afac4ef3ebacd4e04c3e277d22edbddcf16bd95d
Hossam Dahshan <hossam.essam@munjz.com>
2022-07-17 08:34:01 +0200

    feat: added pusher dummy configs to system config.js

--- src/config.js
+++ src/config.js
@@ -9,3 +9,9 @@ export const IS_STAG = process.env.REACT_APP_ENV === "STAGE";
 
 export const SENTRY_KEY = process.env.REACT_APP_SENTRY_KEY;
 export const FRESHDESK_KEY = process.env.REACT_APP_FRESHDESK_KEY;
+
+export const PUSHER_CONFIGS = {
+  app_cluster: "",
+  app_key: "",
+  auth_endpoint: `${API_URL}/pusher/auth`,
+};
