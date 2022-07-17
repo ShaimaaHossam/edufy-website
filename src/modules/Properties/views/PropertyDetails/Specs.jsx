569f13e212e4bcd7dc908c0ed33f1763aa013f62
Hossam Dahshan <hossam.essam@munjz.com>
2022-07-17 08:28:25 +0200

    feat: added property specs/assets with AirTable

--- src/modules/Properties/views/PropertyDetails/Specs.jsx
+++ src/modules/Properties/views/PropertyDetails/Specs.jsx
@@ -1,12 +1,12 @@
 import { useParams } from "react-router-dom";
 
-function Assets() {
+function Specs() {
   const { propertyID } = useParams();
 
   return (
     <iframe
       title="property assets"
-      src={`https://airtable.com/embed/shr6DuSA5b98WkMpg?backgroundColor=transparent&filter_Property%20ID=${propertyID}`}
+      src={`https://airtable.com/embed/shr79f6V8aJMNjiJJ?backgroundColor=transparent&filter_Property%20ID=${propertyID}`}
       width="100%"
       height="540"
       style={{ background: "transparent", border: "none" }}
@@ -14,4 +14,4 @@ function Assets() {
   );
 }
 
-export default Assets;
+export default Specs;
